c8c35d296d5ee34ae46d0ca182cb8225bca9bf22
shahm32 <shahm32@SCE038>
2024-06-06 20:00:06 -0600

    Add project files.

---
+++ Core/Blog.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core
+{
+    public class Blog: BaseEntity
+    {
+        
+        public string Url { get; set; }
+        public bool IsPublic { get; set; }
+    }
+}
